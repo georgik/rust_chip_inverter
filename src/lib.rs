4a5a411c7d535213ab5b7f2938c03f27d16d1332
Juraj Michalek <juraj.michalek@gmail.com>
2024-04-02 16:19:59 +0200

    experiemtal P4 display with GT911

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,7 @@
 use std::ffi::{c_void, CString};
 
 use wokwi_chip_ll::{
-    debugPrint, pinInit, pinRead, pinWatch, pinWrite, PinId, WatchConfig, BOTH, HIGH, INPUT, LOW,
-    OUTPUT,
+    debugPrint, i2cInit, pinInit, pinRead, pinWatch, pinWrite, I2CConfig, PinId, WatchConfig, BOTH, HIGH, INPUT, LOW, OUTPUT
 };
 
 struct Chip {
@@ -29,25 +28,65 @@ pub unsafe fn on_pin_change(user_data: *const c_void, _pin: PinId, value: u32) {
     }
 }
 
+// Handle I2C connect 
+pub unsafe extern "C" fn i2c_connect(user_data: *const c_void, address: u8, connect: bool) {
+    debugPrint(CString::new("I2C Connect").unwrap().into_raw());
+}
+
+// Handle I2C read
+pub unsafe extern "C" fn i2c_read(user_data: *const c_void, address: u8, buffer: *mut u8, length: u32) {
+    debugPrint(CString::new("I2C Read").unwrap().into_raw());
+}
+
+// Handle I2C write
+pub unsafe extern "C" fn i2c_write(user_data: *const c_void, address: u8, buffer: *const u8, length: u32) {
+    debugPrint(CString::new("I2C Write").unwrap().into_raw());
+}
+
+// Handle I2C disconnect
+pub unsafe extern "C" fn i2c_disconnect(user_data: *const c_void, address: u8) {
+    debugPrint(CString::new("I2C Disconnect").unwrap().into_raw());
+    // Print address
+    debugPrint(CString::new(format!("Address: {}", address)).unwrap().into_raw());
+    // Print user_data
+    debugPrint(CString::new(format!("User Data: {}", user_data as usize)).unwrap().into_raw());
+}
+
 #[no_mangle]
 pub unsafe extern "C" fn chipInit() {
     debugPrint(CString::new("Hello Rust!").unwrap().into_raw());
 
-    let chip = Chip {
+    // Configuration for GT911 I2C touch controller
+    let mut chip = Chip {
         pin_in: pinInit(CString::new("IN").unwrap().into_raw(), INPUT),
         pin_out: pinInit(CString::new("OUT").unwrap().into_raw(), OUTPUT),
     };
-    CHIP_VEC.push(chip);
-    let chip = CHIP_VEC.last().unwrap();
-
-    let value = pinRead(chip.pin_in);
-    pinWrite(chip.pin_out, !value);
-
-    let watch_config = WatchConfig {
-        user_data: (CHIP_VEC.len() - 1) as *const c_void,
-        edge: BOTH,
-        pin_change: on_pin_change as *const c_void,
+    let mut config = I2CConfig {
+        sda: pinInit(CString::new("SDA").unwrap().into_raw(), OUTPUT),
+        scl: pinInit(CString::new("SCL").unwrap().into_raw(), OUTPUT),
+        user_data: &mut chip as *mut Chip as *mut c_void,
+        address: 0x14,
+        connect: i2c_connect as *const c_void, // Cast the function pointer to *const c_void
+        read: i2c_read as *const c_void, // Cast the function pointer to *const c_void
+        write: i2c_write as *const c_void,
+        disconnect: i2c_disconnect as *const c_void,
     };
+    i2cInit(&config);
+    // let chip = Chip {
+    //     pin_in: pinInit(CString::new("IN").unwrap().into_raw(), INPUT),
+    //     pin_out: pinInit(CString::new("OUT").unwrap().into_raw(), OUTPUT),
+    // };
+    // CHIP_VEC.push(chip);
+    // let chip = CHIP_VEC.last().unwrap();
 
-    pinWatch(chip.pin_in, &watch_config);
+    // let value = pinRead(chip.pin_in);
+    // pinWrite(chip.pin_out, !value);
+
+    // let watch_config = WatchConfig {
+    //     user_data: (CHIP_VEC.len() - 1) as *const c_void,
+    //     edge: BOTH,
+    //     pin_change: on_pin_change as *const c_void,
+    // };
+
+    // pinWatch(chip.pin_in, &watch_config);
 }
